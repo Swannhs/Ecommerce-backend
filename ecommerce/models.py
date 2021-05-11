71e444c42ac61c0a7ff6ab7d03e7ef5041b53a15
Swannhs <princeapurbask@gmail.com>
2021-05-11 23:54:05 +0600

    Added product rewirw in modle

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -17,3 +17,19 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.rating)
