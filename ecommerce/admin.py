6608817d984861677716b0ea33ab33ea99a346f4
Swannhs <princeapurbask@gmail.com>
2021-05-11 23:57:54 +0600

    Register model in admin section

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from ecommerce.models import Product
+from ecommerce.models import Product, Review, Order, ShippingAddress
 
 admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(ShippingAddress)
