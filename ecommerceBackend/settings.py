7dd7ef883a4c3e43f1a462baa8ea56383b1eb04f
Swannhs <princeapurbask@gmail.com>
2021-05-11 23:06:24 +0600

    Added rest framework

--- ecommerceBackend/settings.py
+++ ecommerceBackend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecommerce.apps.EcommerceConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
